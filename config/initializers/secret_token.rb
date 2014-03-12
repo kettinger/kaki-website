820183e343b43329467e49c5874f2d7d05b2bfed
kettinger <vagrant@precise32.(none)>
2014-03-12 07:49:05 +0000

    first commit for kaki website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KakiWebsite::Application.config.secret_key_base = 'ec11054325cf64351439ec9324c73c01f32c14142715fec582c79610154d344a006edd80e7b5aab4acf40c6efedaacde0455ef576899a6358568e26bbfeda0f1'
